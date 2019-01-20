851a5848cdc92a7acde8b11ebadf8cf6f640795d
AnguleMathias <angulemathias3@gmail.com>
2019-01-20 23:38:55 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,53 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import ApolloClient from 'apollo-boost';
+import {ApolloProvider} from "react-apollo";
+import gql from 'graphql-tag';
 import logo from './logo.svg';
 import './App.css';
 
+const client = new ApolloClient({
+    uri: 'https://api-euwest.graphcms.com/v1/cjr1kgw810n2901dnojl3r4lu/master'
+});
+
+const testQuery = gql`
+    {
+      posts {
+        id
+        title
+        body
+      }
+    }
+`;
+
+client
+    .query({
+        query: testQuery
+    })
+    .then(res => console.log(res));
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <ApolloProvider client={client}>
+                <div className="App">
+                    <header className="App-header">
+                        <img src={logo} className="App-logo" alt="logo"/>
+                        <p>
+                            Edit <code>src/App.js</code> and save to reload.
+                        </p>
+                        <a
+                            className="App-link"
+                            href="https://reactjs.org"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            Learn React
+                        </a>
+                    </header>
+                </div>
+            </ApolloProvider>
+        );
+    }
 }
 
 export default App;
